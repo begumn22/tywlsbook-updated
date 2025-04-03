be3077c188e4358c30a71ddf572e5b732d27a66c
nashida-b <begumn@tywls-astoria.org>
2025-04-03 12:18:42 -0400

    Update ContentView.swift
    Added content to the UniformPolicy view

--- ContentView.swift
+++ ContentView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+//        NavigationStack{
+//            Form{
+//                Section{
+//                    Text("Hello, World!")
+//                }
+//            }
+//        }
         NavigationView{
             HStack{
                 NavigationLink(destination: Policies()) {
@@ -28,7 +35,6 @@ struct ContentView: View {
                     .padding()
                 }
             }
-            Spacer()
         }
     }
 }
@@ -72,8 +78,17 @@ struct Policies: View{
 struct UniformPolicy: View {
     var body: some View {
         NavigationView{
-            HStack{
-                Text("Hi!")
+            ScrollView{
+                VStack{
+                    Text("Uniform Policy").font(.largeTitle)
+                    Text("2023-24").font(.title)
+                    Spacer()
+                    Text("\(Text("Why Uniforms?").foregroundColor(.blue)) \(Text("Mission Driven. Sisterhood. Creates cohesion. Reduces the potential for bullying. Fairer dress codes. Removes peer pressure. Reduces distractions. Focus on character. Economically wise.").italic())")
+                    Spacer()
+                    Text("\(Text("The Young Women’s Leadership School of Astoria").bold()) is an all-girls college-bound school with a mission to create the next generation of extraordinary women. Ann Rubenstein Tisch as the Founder and President of Young Women's Leadership Network (YWLN), an organization that operates The Young Women's Leadership Schools (TYWLS), a network of all-girls public schools which affords us a full-time counselor through CollegeBound Initiative (CBI), a co-ed college access program has created the TYWLS schools with the expectation that we wear our uniforms with pride and solidarity.")
+                    Spacer()
+                    Text("Ann's vision was to provide students growing up in low-income communities with a high-quality college preparatory education modeled upon the finest private schools. We honor the privilege of being a part of a network which affords us a college-bound counselor, college trips, partnerships, after school programs. Our mission at TYWLS Astoria is to encourage community, sisterhood, and respect. Our school uniforms are a part of that mission and all students are required to follow this policy.")
+                }
             }
             Spacer()
         }
@@ -83,8 +98,11 @@ struct UniformPolicy: View {
 struct TechPolicy: View {
     var body: some View {
         NavigationView{
-            HStack{
-                Text("Hello!")
+            ScrollView{
+                VStack{
+                    Text("Hello!").font(.largeTitle)
+                    Text("Year").font(.title)
+                }
             }
             Spacer()
         }
