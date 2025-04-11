aab7e00a4b9d1b2d6195cd3dd402f8154ee19e29
nashida-b <begumn@tywls-astoria.org>
2025-04-11 11:31:40 -0400

    Update ContentView.swift
    I added some text for the Tech & Uniform policies as well as a contact button that opens up an email client that acts as a placeholder for the forum. However, I'm not entirely sure if it works

--- ContentView.swift
+++ ContentView.swift
@@ -20,19 +20,14 @@ struct ContentView: View {
             HStack{
                 NavigationLink(destination: Policies()) {
                     VStack {
-                        NavigationLink(destination: Policies()) {
-                                VStack {
-                                    Image(systemName: "document.on.clipboard")
-                                        .padding()
-                                    Text("Policies")
-                                }
+                      Image(systemName: "document.on.clipboard")
                             .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                            }
+                      Text("Policies")
                     }
                     .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
                 }
             }
         }
@@ -55,6 +50,7 @@ struct Policies: View{
                         .cornerRadius(8)
                         }
                 .padding()
+                
                 
                 NavigationLink(destination: TechPolicy()) {
                             VStack {
@@ -82,12 +78,18 @@ struct UniformPolicy: View {
                 VStack{
                     Text("Uniform Policy").font(.largeTitle)
                     Text("2023-24").font(.title)
-                    Spacer()
-                    Text("\(Text("Why Uniforms?").foregroundColor(.blue)) \(Text("Mission Driven. Sisterhood. Creates cohesion. Reduces the potential for bullying. Fairer dress codes. Removes peer pressure. Reduces distractions. Focus on character. Economically wise.").italic())")
-                    Spacer()
-                    Text("\(Text("The Young Women’s Leadership School of Astoria").bold()) is an all-girls college-bound school with a mission to create the next generation of extraordinary women. Ann Rubenstein Tisch as the Founder and President of Young Women's Leadership Network (YWLN), an organization that operates The Young Women's Leadership Schools (TYWLS), a network of all-girls public schools which affords us a full-time counselor through CollegeBound Initiative (CBI), a co-ed college access program has created the TYWLS schools with the expectation that we wear our uniforms with pride and solidarity.")
-                    Spacer()
-                    Text("Ann's vision was to provide students growing up in low-income communities with a high-quality college preparatory education modeled upon the finest private schools. We honor the privilege of being a part of a network which affords us a college-bound counselor, college trips, partnerships, after school programs. Our mission at TYWLS Astoria is to encourage community, sisterhood, and respect. Our school uniforms are a part of that mission and all students are required to follow this policy.")
+                    Text("\(Text("Why Uniforms?").foregroundColor(.blue)) \(Text("Mission Driven. Sisterhood. Creates cohesion. Reduces the potential for bullying. Fairer dress codes. Removes peer pressure. Reduces distractions. Focus on character. Economically wise.").italic())").bold().padding()
+                    Text("\(Text("The Young Women’s Leadership School of Astoria").bold()) is an all-girls college-bound school with a mission to create the next generation of extraordinary women. Ann Rubenstein Tisch as the Founder and President of Young Women's Leadership Network (YWLN), an organization that operates The Young Women's Leadership Schools (TYWLS), a network of all-girls public schools which affords us a full-time counselor through CollegeBound Initiative (CBI), a co-ed college access program has created the TYWLS schools with the expectation that we wear our uniforms with pride and solidarity.").padding()
+                    Text("Ann's vision was to provide students growing up in low-income communities with a high-quality college preparatory education modeled upon the finest private schools. We honor the privilege of being a part of a network which affords us a college-bound counselor, college trips, partnerships, after school programs. Our mission at TYWLS Astoria is to encourage community, sisterhood, and respect. Our school uniforms are a part of that mission and all students are required to follow this policy.").padding()
+                    Text("Students are required to wear the school uniform each day and P.E. uniform on P.E. days.").bold().italic().padding()
+                    Text("").padding()
+                    Button("Contact Us") {
+                        openEmailClient()
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
                 }
             }
             Spacer()
@@ -100,15 +102,38 @@ struct TechPolicy: View {
         NavigationView{
             ScrollView{
                 VStack{
-                    Text("Hello!").font(.largeTitle)
-                    Text("Year").font(.title)
+                    Text("Student Technology Policy").font(.largeTitle)
+                    Text("2023-2024").font(.title)
+                    Text("We are a very unique school in that all of our students are one to one with Apple laptops!").bold()
+                    Text("With that comes a lot of responsibility.").padding()
+                    Text("TYWLS Astoria’s expectations and school technology policy is as follows:").bold().italic()
+                    Text("1. \(Text("Laptops (& any technology-assigned equipment) \(Text("MUST ONLY be used for school or academic use").underline())").bold()) both in school and at home. Students cannot use their school laptops for social media, YouTube, watching movies, Google Hangout etc. If a student is found to be misusing this privilege, consequences can vary depending on the nature of misuse (see below).").padding()
+                    Text("2. Students are responsible to keep the laptops clean and to keep liquids away from it at all times, as they have signed the contract accepting that responsibility. Students are expected to take care of the laptop, and keep track of it at all times. Students are \(Text("NOT allowed to loan or give their laptops to other students or siblings.").italic().underline()) For example, if a student asks another student to use their laptop, this is absolutely NOT allowed.").padding()
                 }
+                Button("Contact Us") {
+                    openEmailClient()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                
             }
             Spacer()
         }
     }
 }
 
+func openEmailClient() {
+        let email = "support@example.com"
+        let subject = "Regarding Policy "
+        let body = "Hi, this is my opinion on policy xyz..."
+        let urlString = "mailto:\(email)?subject=\(subject)&body=\(body)"
+
+        if let url = URL(string: urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "") {
+            UIApplication.shared.open(url)
+        }
+    }
 #Preview {
     ContentView()
 }
